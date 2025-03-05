58acfa9e20250285a4cd9f79aeaae28f3382519e
Andrew Hayes <andrewhayes248@gmail.com>
2025-03-05 14:03:30 -0600

    Add project files.

---
+++ MidEx4/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidEx4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IDictionary<string, int> Contacts = new Dictionary<string, int>();
+            bool exit = false;
+            while (!exit)
+            {
+                Console.WriteLine("1. Add Contact");
+                Console.WriteLine("2. Remove a Contact");
+                Console.WriteLine("3. Search Contact");
+                Console.WriteLine("4. Display All Contacts");
+                Console.WriteLine("5. Exit");
+
+                int choice = 0;
+
+                switch (choice)
+                {
+                    case 1:
+                        AddContact();
+                        break;
+                    case 2:
+                        RemoveContact();
+                        break;
+                    case 3:
+                        SearchContact();
+                        break;
+                    case 4:
+                        DisplayContacts();
+                        break;
+                    case 5:
+                        exit = true;
+                        return;
+
+                }
+            }
+        }
+        static void AddContact()
+        {
+            Console.WriteLine("Enter Name: ");
+            string name = Console.ReadLine();
+            if (Contacts.ContainsKey(name))
+            {
+                Console.WriteLine("Contact already exists");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Enter Phone Number: ");
+                int phone = Convert.ToInt32(Console.ReadLine());
+                Contacts.Add(name, phone);
+            }
+                
+        }
+        static void RemoveContact()
+        {
+            Console.WriteLine("Enter Name: ");
+            string name = Console.ReadLine();
+            if (Contacts.ContainsKey(name))
+            {
+                Contacts.Remove(name);
+            }
+            else
+            {
+                Console.WriteLine("Contact does not exist");
+            }
+        }
+        static void SearchContact()
+        {
+            Console.WriteLine("Would you like to search by name or phone number?");
+            string search = Console.ReadLine();
+
+            if (search == "name")
+            {
+                Console.WriteLine("Enter Name: ");
+                string name = Console.ReadLine();
+                if (Contacts.ContainsKey(name))
+                {
+                    Console.WriteLine("Phone Number: " + Contacts[name]);
+                }
+                else
+                {
+                    Console.WriteLine("Contact does not exist");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Enter Phone Number: ");
+                string number = Console.ReadLine();
+                if (Contacts.ContainsKey(number))
+                {
+                    Console.WriteLine("Phone Number: " + Contacts[number]);
+                }
+                else
+                {
+                    Console.WriteLine("Contact does not exist");
+                }
+            }
+        }
+
+        static void DisplayContacts()
+        {
+            foreach (var contact in Contacts)
+            {
+                Console.WriteLine("Name: " + contact.Key + " Phone Number: " + contact.Value);
+            }
+        }
+    }
+}
